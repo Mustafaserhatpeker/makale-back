377ebab1b819c8813e4fdc5cb181b9ac3b6cbd8f
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-11 16:25:38 +0300

    first commit

---
+++ back/server.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // JSON veri kullanımı için middleware
+
+// Basit bir test endpointi
+app.get("/", (req, res) => {
+  res.send("Merhaba, Express API çalışıyor!");
+});
+
+// Sunucuyu başlat
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
