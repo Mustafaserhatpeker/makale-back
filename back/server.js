37b0e97963c06f5a3153738c91f47583e8cf844e
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-12 21:51:11 +0300

    feat: add CORS support and update server endpoint

--- back/server.js
+++ back/server.js
@@ -1,12 +1,27 @@
 import express, { json } from "express";
+import cors from "cors";
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const users = [
+  {
+    id: 1,
+    name: "Ahmet",
+    email: "a@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Mehmet",
+    email: "m@gmail.com",
+  },
+];
 
 // Basit bir test endpointi
-app.get("/", (req, res) => {
-  res.send("Merhaba, Express API çalışıyor!");
+app.get("/merhaba", (req, res) => {
+  res.send(users);
 });
 
 // Sunucuyu başlat
