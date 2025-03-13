b687b487456f5f835dee5f86e0819a5d8ed73e9d
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-14 02:56:23 +0300

    feat: add user registration endpoint in admin controller

--- back/src/controllers/adminController.js
+++ back/src/controllers/adminController.js
@@ -1,10 +1,20 @@
-import { adminLogin } from "../services/adminService.js";
+import { adminLogin, addUser } from "../services/adminService.js";
 
 export const loginAdmin = async (req, res) => {
   try {
     const { username, password } = req.body;
     const { admin, token } = await adminLogin(username, password);
     res.json({ message: "Login successful", admin, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await addUser(name, email, password);
+    res.json({ message: "User Added", user });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
