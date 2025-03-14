dd72fd6bec1fa57b07a4e67818145dd438cd266e
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-14 04:26:06 +0300

    feat: add admin middleware for token validation and role check in admin routes

--- back/src/middleware/authMiddleware.js
+++ back/src/middleware/authMiddleware.js
@@ -22,8 +22,22 @@ const authMiddleware = (req, res, next) => {
 };
 
 const adminMiddleware = (req, res, next) => {
-  if (req.user.userRole !== "admin") {
-    return res.status(403).json({ message: "Access Denied. Admin only." });
+  const token = req.header("Authorization");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied. No token provided." });
+  }
+  try {
+    const decoded = jwt.verify(
+      token.replace("Bearer ", ""),
+      process.env.JWT_SECRET
+    );
+    if (decoded.userRole !== "admin") {
+      return res.status(403).json({ message: "Access Denied. Admin only." });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid token." });
   }
   next();
 };
--- back/src/routes/adminRoutes.js
+++ back/src/routes/adminRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { loginAdmin, registerUser } from "../controllers/adminController.js";
-
+import { adminMiddleware } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/login", loginAdmin);
-router.post("/add-user", registerUser);
+router.post("/add-user", adminMiddleware, registerUser);
 
 export default router;
