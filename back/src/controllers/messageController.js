880e5df0668594543ada77c71cd84700d9abf80c
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-19 17:18:26 +0300

    feat: rename addUser to addMessage and implement message registration in controller

---
+++ back/src/controllers/messageController.js
@@ -0,0 +1,11 @@
+import { addMessage } from "../services/messageService.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { sender, receiver, message } = req.body;
+    const mess = await addMessage(sender, receiver, message);
+    res.json({ message: "Kullanıcı Başarıyla Eklendi.", mess });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- back/src/services/messageService.js
+++ back/src/services/messageService.js
@@ -1,5 +1,5 @@
 import Message from "../models/Message.js";
-export const addUser = async (sender, receiver, message) => {
+export const addMessage = async (sender, receiver, message) => {
   const message = new Message({ sender, receiver, message });
   await message.save();
   return message;
