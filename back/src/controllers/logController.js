f4e3cd3168ac9bf98dda40fb1e6ee5da0b6c7747
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-06 00:54:17 +0300

    fix: remove unnecessary line break in getLogsController for cleaner code

--- back/src/controllers/logController.js
+++ back/src/controllers/logController.js
@@ -3,12 +3,12 @@ import { getLogs } from "../services/logService.js";
 export const getLogsController = async (req, res) => {
     try {
         const { logType, logState, sortByDate } = req.body;
+        console.log(req.body);
         const logs = await getLogs({ logType, logState, sortByDate });
 
         if (!logs || logs.length === 0) {
             return res.status(404).json({ message: "No logs found." });
         }
-
         res.json({ logs });
     } catch (error) {
         res.status(500).json({ error: error.message });
