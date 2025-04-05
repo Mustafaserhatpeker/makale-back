fd84ef158b1f33983593eef00ef07a7799fa0b36
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-05 22:07:37 +0300

    feat: add logging for message retrieval failures in getMessagesByFileId and getAllMessages functions

--- back/src/services/messageService.js
+++ back/src/services/messageService.js
@@ -1,5 +1,5 @@
 import Message from "../models/Message.js";
-
+import Log from "../models/Log.js";
 export const saveMessage = async (fileId, sender, message) => {
   const newMessage = new Message({ fileId, sender, message });
   await newMessage.save();
@@ -7,11 +7,33 @@ export const saveMessage = async (fileId, sender, message) => {
 };
 
 export const getMessagesByFileId = async (fileId) => {
-  return await Message.find({ fileId });
+
+  const message = await Message.find({ fileId });
+  if (!message) {
+    const log = new Log({
+      logContent: "Mesaj bulunamadı.",
+      logType: "info",
+      logState: "Mesaj İşlemleri",
+    });
+    await log.save();
+    return null;
+  }
+
+  return message;
 };
 
 export const getAllMessages = async () => {
   const messages = await Message.find().sort({ createdAt: -1 });
+
+  if (!messages || messages.length === 0) {
+    const log = new Log({
+      logContent: "Mesaj bulunamadı.",
+      logType: "info",
+      logState: "Mesaj İşlemleri",
+    });
+    await log.save();
+    return null;
+  }
 
   const latestMessages = {};
 
